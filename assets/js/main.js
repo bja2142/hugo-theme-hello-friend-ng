6c6562c900af724013e1a357775ce66e562a2e17
Ben <git@jianmin.dev>
2021-10-15 18:44:04 +0000

    theme updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ function detectOSColorTheme() {
     document.documentElement.setAttribute("data-theme", "light");
   } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
     document.documentElement.setAttribute("data-theme", "dark");
-  } if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+  } else if (window.matchMedia("(prefers-color-scheme: light)").matches) {
     document.documentElement.setAttribute("data-theme", "light");
   } else {
     document.documentElement.setAttribute("data-theme", "dark");
